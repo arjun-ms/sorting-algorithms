48ac2ec7da118f77fcd29165e6e6375c8155f64f
Arjun M S <64315213+arjun-ms@users.noreply.github.com>
2025-09-29 08:45:10 +0530

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -8,3 +8,12 @@ def bubble_sort(arr):
                 # swap if elements are out of order
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
+
+
+"""
+Bubble Sort: Imagine kids standing in a line, and they keep comparing themselves with the one next to them. 
+If someone is taller than the one beside them, they swap places. 
+After a lot of swapping rounds, the tallest kid “bubbles up” to the end, then the second tallest, and so on.
+
+👉 It repeatedly swaps neighbors until the list is sorted.
+"""
